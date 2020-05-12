562b3b73be895565c4561a604791ccbad9139e43
NandithaNandanavanam <64999992+NandithaNandanavanam@users.noreply.github.com>
2020-05-11 23:16:29 -0400

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -3,32 +3,34 @@ import tkinter.filedialog as fdialog
 import cv2
 import Voice
 import capture
+import time
 
-def browseimage():
-    filename = fdialog.askopenfilename()
-    img = cv2.imread(filename)
-    img=cv2.resize(img,(350,350))
-    cv2.imwrite('face.png',img)
-    photo=PhotoImage(file='face.png')
-    
-    canvas.delete("all")
-    canvas2 = canvas.create_image(1,1,anchor=NW, image=photo)
-    canvas.itemconfig(canvas1, image = canvas2)
-    canvas.pack()
-    leftframe.pack(side=LEFT)
-    root.mainloop()
-    
-def voicemodule():
-    voicecommand, error = Voice.voice_module()
-    
-    if error ==0:
-        label1 = Label(rightframe, text="You said : " + voicecommand)
-    else:
-        label1 = Label(rightframe, text=voicecommand)
+def verify_voice_command():
+    print("Verifying")
+    while True:
+        label_voicetest.configure(text="Is this what you said? Say Yes or No")
+        label_voicetest.place()
+        
+        root.update_idletasks()
+        root.update()
+        
+        voiceverify = Voice.voice_module()
+        if voiceverify=="yes":
+            print("Verified")
+            label_voicetest.configure(text="Speak Anything")
+            label_voicetest.place()
+            break
+        elif voiceverify=="no":
+            print("Not correct")
+            label_voicetest.configure(text="Speak Anything")
+            label_voicetest.place()
+            break
 
-    label1.pack()
-    rightframe.pack(side=RIGHT)
-    root.mainloop()
+def display_chat(voicecommand,rownum):
+    rownum=rownum+1
+    Label(rightframe, text=time.strftime("%H:%M:%S"), bg='white',fg='black',bd=3).grid(row=rownum,column=0,padx=5, pady=5)
+    rownum=rownum+1
+    Label(rightframe, text="You said : " + voicecommand, bg='white',fg='black',bd=3).grid(row=rownum,column=0,padx=5, pady=5)
     
 def captureimage():
     img = capture.capture_image()
@@ -40,48 +42,75 @@ def captureimage():
     canvas2 = canvas.create_image(1,1,anchor=NW, image=photo)
     canvas.itemconfig(canvas1, image = canvas2)
     canvas.pack()
-    leftframe.pack(side=LEFT)
-    root.mainloop()
-    
+    root.pack(side=LEFT)
+       
 #GUI
-#Create window
-root = Tk()
-root.title("Face enabled Time clock")
-root.geometry("910x550")
+root = Tk() 
+root.title("ClockIn/ClockOut Application") 
+root.config(bg="beige") 
+ 
+leftframe = Frame(root, width=200, height=400, bg='beige')
+leftframe.grid(row=0, column=0, padx=10, pady=5)
+rightframe = Frame(root, width=650, height=400, bg='beige')
+rightframe.grid(row=0, column=1, padx=10, pady=5)
 
-#Create topframe, bottomframe, leftframe and rightframe
-topframe=Frame(root)
-topframe.pack(side=TOP)
+label_voice = Label(leftframe,text="")
+label_voice.grid(row=5,column=0, padx=5, pady=5)
 
-bottomframe=Frame(root)
-bottomframe.pack(side=BOTTOM)
+label_voicetest = Label(leftframe,text="Speak Anything",bg="beige")
+label_voicetest.grid(row=4,column=0, padx=5, pady=5)
+ 
+while True:
+    root.update_idletasks()
+    root.update()
+     
+    image = PhotoImage(file="gecap.png") 
+    image = image.subsample(3,3)
+    Label(leftframe, image=image).grid(row=0,column=0, padx=5, pady=5)
+    Label(leftframe, text="Welcome to the Touch less Face enabled Clock In/Clock Out System. Say LOGIN to Clock In, EXIT to close", bg='beige',bd=3).grid(row=1, column=0, padx=5, pady=5)
 
-leftframe=Frame(topframe,bg='black')
-leftframe.pack(side=LEFT)
-
-rightframe=Frame(bottomframe)
-rightframe.pack(side=RIGHT)
-
-canvas=Canvas(leftframe,width=327,height=363)
-canvas.pack()
-photo=PhotoImage(file='gecap.png')
-canvas1=canvas.create_image(20,10,anchor=NW, image=photo)
-leftframe.pack(side=LEFT)
+    root.update_idletasks()
+    root.update()
     
-#buttons
-Bt1=Button(rightframe,text="Capture Image",background="LightBlue", bd=0,width=15,height=1,command=captureimage)
-Bt1.pack(fill=X,pady=10)
+    Label(rightframe, text="Chat with the System", bg='beige',fg='black',bd=3).grid(row=0,column=0,padx=5, pady=5) 
+    Label(rightframe, text=time.strftime("%H:%M:%S"), bg='beige',fg='black',bd=3).grid(row=1,column=0,padx=5, pady=5)
+    Label(rightframe, text="System: Hello, What do you want to do?", bg='beige',fg='black',bd=3).grid(row=2,column=0,padx=5, pady=5)
+    
+    root.update_idletasks()
+    root.update()
+    
+    voicecommand = Voice.voice_module()
+    label_voice.configure(text="You said : " + voicecommand)
+    label_voice.place()
+    
+    root.update_idletasks()
+    root.update()
+    
+    verify_voice_command()
+    
+    rownum=2
+    display_chat(voicecommand,rownum)
+    
+    if voicecommand=="login":
+        Label(rightframe, text=time.strftime("%H:%M:%S"), bg='white',fg='black',bd=3).grid(row=5,column=0,padx=5, pady=5)
+        Label(rightframe, text="Okay!Look at the camera", bg='white',fg='black',bd=3).grid(row=6,column=0,padx=5, pady=5)
+    elif voicecommand=="exit":
+        break
+    else:
+        continue
+   
+   
+  
+    
+    
+    
+    
+  
 
-Bt2=Button(rightframe,text="Capture Voice",background="LightBlue", bd=0,width=15,height=1,command=voicemodule)
-Bt2.pack(fill=X,pady=10)
 
-Bt3=Button(rightframe,text="Exit",background="LightBlue", bd=0,width=15,height=1,command=root.destroy)
-Bt3.pack(fill=X,pady=10)
 
-rightframe.pack(side=RIGHT)
 
-#stable main window on infinity time
-root.mainloop()
+
 
        
 
