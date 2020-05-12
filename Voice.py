8578223ca8f2e78d3e48531461105cf81fcb026e
NandithaNandanavanam <64999992+NandithaNandanavanam@users.noreply.github.com>
2020-05-11 23:16:56 -0400

    Add files via upload

--- Voice.py
+++ Voice.py
@@ -2,17 +2,18 @@ import speech_recognition as sr
 import pyaudio
 
 def voice_module():
-    err = 0
+    
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Speak Anything :")
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
+            print("You said:"+text)
         except:
             text= "Sorry could not recognize what you said"
-            err = 1
-    return text,err
+            
+    return text
     
 
 
